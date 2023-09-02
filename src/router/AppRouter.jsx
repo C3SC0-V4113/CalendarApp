5b3238f83c1328bae4ed3a0cdca8173aa960850c
CESCO <cescovalledev@gmail.com>
2023-09-01 22:23:55 -0600

    first calls

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,32 @@
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import { LoginPage } from "../auth/pages/LoginPage";
+import { CalendarPage } from "../calendar/pages";
+
+export const AppRouter = () => {
+  const authStatus = "not-authenticated";
+
+  const router = createBrowserRouter(
+    authStatus === "not-authenticated"
+      ? [
+          {
+            path: "/auth/*",
+            element: <LoginPage />,
+          },
+        ]
+      : [
+          {
+            path: "/",
+            element: <CalendarPage />,
+          },
+          {
+            path: "/*",
+            element: <Navigate to={"/auth/*"} />,
+          },
+        ]
+  );
+  return <RouterProvider router={router} />;
+};

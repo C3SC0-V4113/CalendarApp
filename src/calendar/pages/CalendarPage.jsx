b12218c703d015a383baf4ad69cb2fd6fe061514
CESCO <cescovalledev@gmail.com>
2023-09-02 22:01:58 -0600

    added calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,47 @@
 import { Navbar } from "../components/Navbar";
 
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const events = [
+  {
+    title: "Cumpleaños de Karla",
+    notes: "Comprar los audifonos",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _uid: "123",
+      name: "Francisco",
+    },
+  },
+];
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc( 100vh - 80px)" }}
+      />
     </>
   );
 };

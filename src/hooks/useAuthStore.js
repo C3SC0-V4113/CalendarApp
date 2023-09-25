134f267122d8ca5c2b9d2c77e89779cf4e4e3b43
CESCO <cescovalledev@gmail.com>
2023-09-24 22:29:33 -0600

    using dispatch to change the states

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,18 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
 import caledarApi from "../api/calendarApi";
+import {
+  clearErrorMessage,
+  onChecking,
+  onLogin,
+  onLogout,
+} from "../store/auth";
 
 export const useAuthStore = () => {
   const dispatch = useDispatch();
   const { status, user, errorMessage } = useSelector((state) => state.auth);
 
   const startLogin = async ({ email, password }) => {
-    console.log({ email, password });
-
+    dispatch(onChecking());
     try {
-      const resp = await caledarApi.post("/auth", { email, password });
-      console.log(resp);
+      const { data } = await caledarApi.post("/auth", { email, password });
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
     } catch (error) {
-      console.error(error);
+      dispatch(onLogout("Credenciales Incorrectas"));
+      setTimeout(() => {
+        dispatch(clearErrorMessage());
+      }, 10);
     }
   };
 

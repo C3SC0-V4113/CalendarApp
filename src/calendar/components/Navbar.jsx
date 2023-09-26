a4b062272ef6ed41202e22f7ff3c311d71cfe433
CESCO <cescovalledev@gmail.com>
2023-09-26 14:27:13 -0600

    error in slice

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
   return (
     <div className="navbar navbar-dark bg-dark mb-4 px-4">
       <span className="navbar-brand">
-        <i className="fas fa-calendar-alt"></i>&nbsp;{user.payload.name}
+        <i className="fas fa-calendar-alt"></i>&nbsp;{user.name}
       </span>
       <button className="btn btn-outline-danger" onClick={startLogout}>
         <i className="fas fa-sign-out-alt"></i>

e33b91242706f61e081b3af964ad2d957f64f4ef
CESCO <cescovalledev@gmail.com>
2023-10-04 17:44:56 -0600

    testing for fabDelete begins

--- tests/calendar/components/FabDelete.test.jsx
+++ tests/calendar/components/FabDelete.test.jsx
@@ -1,15 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import { FabDelete } from "../../../src/calendar/components/FabDelete";
-import { Provider } from "react-redux";
-import { store } from "../../../src/store";
+import { useCalendarStore } from "../../../src/hooks";
+
+jest.mock("../../../src/hooks/useCalendarStore");
 
 describe("pruebas en <FabDelete/>", () => {
   test("debe de mostrar el componente correctamente", () => {
-    render(
-      <Provider store={store}>
-        <FabDelete />
-      </Provider>
-    );
-    screen.debug();
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: false,
+    });
+
+    render(<FabDelete />);
+
+    const btn = screen.getByLabelText("btn-delete");
+    expect(btn.classList).toContain("btn");
+    expect(btn.classList).toContain("btn-danger");
+    expect(btn.classList).toContain("fab-danger");
+    expect(btn.style.display).toBe("none");
   });
 });

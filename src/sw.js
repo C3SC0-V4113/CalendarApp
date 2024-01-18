8f42e2a7513c022c8ea2a3162042355dfcc208d0
CESCO <cescovalledev@gmail.com>
2024-01-18 16:12:36 -0600

    saving in cache certain routes

--- src/sw.js
+++ src/sw.js
@@ -26,6 +26,8 @@ self.addEventListener("install", async (event) => {
   const cache = await caches.open("cache-1");
 
   await cache.addAll([
-    //aca lo que se quiere llevar al cache
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css",
+    "/vite.svg",
   ]);
 });

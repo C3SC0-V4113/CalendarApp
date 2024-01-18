37eff8b6e98bf64730f1b5bebaa4cedcc52eac92
CESCO <cescovalledev@gmail.com>
2024-01-17 22:41:21 -0600

    service worker personalized added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,19 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+        type: "module",
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+      },
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.js",
+    }),
+  ],
 });

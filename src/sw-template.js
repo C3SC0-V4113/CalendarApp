cf9283fd736e4aa0f982be824124564e0df78c6b
CESCO <cescovalledev@gmail.com>
2024-01-19 22:33:01 -0600

    events cache doesnt work

--- src/sw-template.js
+++ src/sw-template.js
@@ -3,3 +3,29 @@ importScripts(
 );
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+workbox.routing.registerRoute(
+  new RegExp(
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+  ),
+  new workbox.strategies.CacheFirst()
+);
+
+workbox.routing.registerRoute(
+  new RegExp(
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+  ),
+  new workbox.strategies.CacheFirst()
+);
+
+workbox.routing.registerRoute(
+  new RegExp("http://localhost:4000/api/events"),
+  new workbox.strategies.NetworkFirst({
+    cacheName: "events-calendar-cache",
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp("http://localhost:4000/api/auth/renew"),
+  new workbox.strategies.NetworkFirst()
+);

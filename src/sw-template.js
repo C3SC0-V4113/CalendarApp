60cfde3795dba914c160d0982f280770ace32bcb
CESCO <cescovalledev@gmail.com>
2024-01-23 17:50:49 -0600

    returning to FH way

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,31 +1,31 @@
-import { registerRoute } from "workbox-routing";
-import { CacheFirst, NetworkFirst } from "workbox-strategies";
-import { precacheAndRoute } from "workbox-precaching";
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
+);
 
-precacheAndRoute(self.__WB_MANIFEST);
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-registerRoute(
+workbox.routing.registerRoute(
   new RegExp(
     "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
   ),
-  new CacheFirst()
+  new workbox.strategies.CacheFirst()
 );
 
-registerRoute(
+workbox.routing.registerRoute(
   new RegExp(
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
   ),
-  new CacheFirst()
+  new workbox.strategies.CacheFirst()
 );
 
-registerRoute(
+workbox.routing.registerRoute(
   new RegExp("http://localhost:4000/api/events"),
-  new NetworkFirst({
+  new workbox.strategies.NetworkFirst({
     maxAgeSeconds: 60, // Establece el tiempo máximo que se almacenará en caché antes de intentar la red nuevamente
   })
 );
 
-registerRoute(
+workbox.routing.registerRoute(
   new RegExp("http://localhost:4000/api/auth/renew"),
-  new NetworkFirst()
+  new workbox.strategies.NetworkFirst()
 );

29a077109cfc66bc6802c5e9ec25c1066b7584ba
CESCO <cescovalledev@gmail.com>
2024-01-27 14:47:45 -0600

    register routes automatized with arraus

--- src/sw-template.js
+++ src/sw-template.js
@@ -6,31 +6,36 @@ workbox.loadModule("workbox-background-sync");
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-workbox.routing.registerRoute(
-  new RegExp(
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
-  ),
-  new workbox.strategies.CacheFirst()
-);
+const cacheNetworkFirst = ["/api/events", "/api/auth/renew"];
 
-workbox.routing.registerRoute(
-  new RegExp(
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-  ),
-  new workbox.strategies.CacheFirst()
-);
+const cacheFirst = [
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css",
+];
 
-workbox.routing.registerRoute(
-  new RegExp("http://localhost:4000/api/events"),
-  new workbox.strategies.NetworkFirst({
-    maxAgeSeconds: 60, // Establece el tiempo máximo que se almacenará en caché antes de intentar la red nuevamente
-  })
-);
+workbox.routing.registerRoute(({ request, url }) => {
+  if (cacheNetworkFirst.includes(url.pathname)) {
+    return true;
+  }
+}, new workbox.strategies.NetworkFirst());
 
-workbox.routing.registerRoute(
-  new RegExp("http://localhost:4000/api/auth/renew"),
-  new workbox.strategies.NetworkFirst()
-);
+workbox.routing.registerRoute(({ request, url }) => {
+  if (cacheFirst.includes(url.href)) {
+    return true;
+  }
+}, new workbox.strategies.CacheFirst());
+
+// workbox.routing.registerRoute(
+//   new RegExp("http://localhost:4000/api/events"),
+//   new workbox.strategies.NetworkFirst({
+//     maxAgeSeconds: 60, // Establece el tiempo máximo que se almacenará en caché antes de intentar la red nuevamente
+//   })
+// );
+
+// workbox.routing.registerRoute(
+//   new RegExp("http://localhost:4000/api/auth/renew"),
+//   new workbox.strategies.NetworkFirst()
+// );
 
 /** Offline Post */
 

aee3b7119b8df39cf86ace51d1029501858aa9db
CESCO <cescovalledev@gmail.com>
2024-01-27 18:48:08 -0600

    useOnlineStatus

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,12 +1,16 @@
-import { useAuthStore } from "../../hooks";
+import { useAuthStore, useOnlineStatus } from "../../hooks";
 
 export const Navbar = () => {
   const { startLogout, user } = useAuthStore();
+  const { isOnline } = useOnlineStatus();
 
   return (
     <div className="navbar navbar-dark bg-dark mb-4 px-4">
       <span className="navbar-brand">
         <i className="fas fa-calendar-alt"></i>&nbsp;{user.name}
+      </span>
+      <span className={isOnline ? "text-success" : "text-danger"}>
+        {isOnline ? "En Linea" : "Fuera de Linea - Peticiones serán guardadas"}
       </span>
       <button className="btn btn-outline-danger" onClick={startLogout}>
         <i className="fas fa-sign-out-alt"></i>
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export * from "./useAuthStore";
 export * from "./useCalendarStore";
 export * from "./useForm";
 export * from "./useUiStore";
+export * from "./useOnlineStatus";
